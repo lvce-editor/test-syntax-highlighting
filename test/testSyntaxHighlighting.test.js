7ffc41e950f3a017c1ef6d2417a80fae3902914f
Le Vivilet <levivlet@pm.me>
2022-06-26 15:01:23 +0200

    fix failing test

--- test/testSyntaxHighlighting.test.js
+++ test/testSyntaxHighlighting.test.js
@@ -53,11 +53,9 @@ test('sample.error-tokenize-path-not-found', async () => {
   const result = await testSample('sample.error-tokenize-path-not-found')
   expect(result.exitCode).toBe(1)
   expect(result.stdout).toBe(``)
-  expect(result.stderr).toMatch(`node:internal/errors:466
-    ErrorCaptureStackTrace(err);
-    ^
-
-Error [ERR_MODULE_NOT_FOUND]: Cannot find module `)
+  expect(result.stderr).toMatch(
+    /Error \[ERR_MODULE_NOT_FOUND\]: Cannot find module .*\/src\/tokenizeDotenv\.js/
+  )
 })
 
 test('sample.error-no-test-cases', async () => {
