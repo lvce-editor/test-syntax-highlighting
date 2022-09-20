6bf8d5e669e109fc33cf3b73093a1609296553f4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-20 16:58:36 +0200

    fix: improve error handling when file is not found (#10)

--- test/testSyntaxHighlighting.test.js
+++ test/testSyntaxHighlighting.test.js
@@ -53,9 +53,7 @@ test('sample.error-tokenize-path-not-found', async () => {
   const result = await testSample('sample.error-tokenize-path-not-found')
   expect(result.exitCode).toBe(1)
   expect(result.stdout).toBe(``)
-  expect(result.stderr).toMatch(
-    /Error \[ERR_MODULE_NOT_FOUND\]: Cannot find module .*tokenizeDotenv\.js/
-  )
+  expect(result.stderr).toMatch(`tokenizer file not found:`)
 })
 
 test('sample.error-no-test-cases', async () => {
