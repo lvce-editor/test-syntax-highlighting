9ea45f5e2f13e94735a354c8686c23f9f6c75743
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-07-16 21:58:11 +0200

    fix: create deep copy of inital state to avoid state from one test af… (#33)

--- src/parts/TokenizeLines/TokenizeLines.js
+++ src/parts/TokenizeLines/TokenizeLines.js
@@ -1,11 +1,10 @@
+import * as GetInitialLineState from '../GetInitialLineState/GetInitialLineState.js'
 import * as GetTokenNames from '../GetTokenNames/GetTokenNames.js'
 import * as JoinLines from '../JoinLines/JoinLines.js'
 import * as SplitLines from '../SplitLines/SplitLines.js'
 
 export const tokenizeLines = (text, Tokenizer) => {
-  let lineState = {
-    ...Tokenizer.initialLineState,
-  }
+  let lineState = GetInitialLineState.getInitialLineState(Tokenizer)
   const tokens = []
   const lines = SplitLines.splitLines(text)
   for (let i = 0; i < lines.length; i++) {
